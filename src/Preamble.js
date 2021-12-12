280c6237c322292961c281744d0d78734aaac404
root <kimyoungjin1001@gmail.com>
2021-12-12 07:08:04 +0000

    G

--- src/Preamble.js
+++ src/Preamble.js
@@ -8,23 +8,23 @@ function Preamble() {
   const theme = useTheme();
   return (
     <>
-      <Typography variant="h6">
-        1. Select payment method from below <br/>
-        2. Complete the attached form <br/>
-        <Typography variant="caption">
-        Information from the form will allow us to record the source of the funds
-        accurately and use them for their intended purpose. <br />
-        Your information will not be published publicly without your permission
-        and your identity will be kept confidential. <br />
-        <br />
-        </Typography>
+      {/* <Typography variant="h6"> */}
+      {/*   1. Select payment method from below <br/> */}
+      {/*   2. Complete the attached form <br/> */}
+      {/*   <Typography variant="caption"> */}
+      {/*   Information from the form will allow us to record the source of the funds */}
+      {/*   accurately and use them for their intended purpose. <br /> */}
+      {/*   Your information will not be published publicly without your permission */}
+      {/*   and your identity will be kept confidential. <br /> */}
+      {/*   <br /> */}
+      {/*   </Typography> */}
 
-        3. Follow the instructions displayed when the form is submitted
+      {/*   3. Follow the instructions displayed when the form is submitted */}
 
-        Submission of the form only indicates intention to donate. 
-        You will receive an email from us confirming your intention,
-        and a second email to confirm donation when we successfully receive your donation.
-      </Typography>
+      {/*   Submission of the form only indicates intention to donate. */} 
+      {/*   You will receive an email from us confirming your intention, */}
+      {/*   and a second email to confirm donation when we successfully receive your donation. */}
+      {/* </Typography> */}
       <Typography
         variant="body2"
         css={{ color: theme.palette.text.secondary }}
